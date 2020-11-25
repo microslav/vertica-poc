3fc67ed448e8b39798cc9b0837dd1d8e82c74983
microslav <miroslavk@upenn.alumni.edu>
2020-11-25 02:22:22 -0800

    Fixed and documented the ephemeral storage script

--- outposts-ephemeral.sh
+++ outposts-ephemeral.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+############################################################
+# Script for using the ephemeral instance storage as Depot
+#
+# If using instances with ephemeral devices, pause the
+# playbook after the *common* role completes. Then manually:
+#   ansible vertica_nodes -m copy -a 'src=./outposts-ephemeral.sh dest=/tmp/ mode=0744'
+#   ansible vertica_nodes -m shell -a '/tmp/outposts-ephemeral.sh'
+#   ansible vertica_nodes -m shell -a 'df -h /home/dbadmin/depot/'
+############################################################
+
 DBUSER="dbadmin"
 DBGRP="verticadba"
 ISTO_DEV="$(nvme list | grep 'Instance Storage' | awk '{print $1}')"
@@ -7,8 +17,9 @@ MPATH="/home/${DBUSER}/depot/"
 
 ### Create a script that will format and mount the ephemeral instance storage
 cat <<_EOF_ > /usr/local/sbin/use-ephemeral.sh
+#!/bin/bash
 # Format the device
-mkfs.xfs -E nodiscard $ISTO_DEV
+mkfs.xfs -f -K $ISTO_DEV
 # Prep and mount the filesystem
 mkdir -p $MPATH
 mount -o discard $ISTO_DEV $MPATH
@@ -32,5 +43,7 @@ _EOF_
 ### Restart and check
 systemctl daemon-reload
 systemctl restart instance-storage
+systemctl enable instance-storage
 systemctl status instance-storage
+sleep 3
 df -h $MPATH
